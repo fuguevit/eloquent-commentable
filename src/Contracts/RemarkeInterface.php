9cd673beef8e81429293cf802c6a693a70185ef0
wangxuan <wangxuan@qgpic.com>
2016-10-29 15:58:06 +0800

    remarkableTrait

--- src/Contracts/RemarkeInterface.php
+++ src/Contracts/RemarkeInterface.php
@@ -4,5 +4,29 @@ namespace Fuguevit\Commentable\Contracts;
 
 interface RemarkeInterface
 {
+    /**
+     * Return all remarks under the entity namespace.
+     *
+     * @return mixed
+     */
+    public static function allRemarks();
+
+    /**
+     * Return MorphToMany Relation of the entity.
+     *
+     * @return mixed
+     */
+    public function remarks();
+
+    /**
+     * Attach the given remark to the entity.
+     *
+     * @param $userId
+     * @param $content
+     *
+     * @return mixed
+     */
+    public function addRemark($userId, $content);
+
 
 }
--- src/Traits/RemarkableTrait.php
+++ src/Traits/RemarkableTrait.php
@@ -2,8 +2,65 @@
 
 namespace Fuguevit\Commentable\Traits;
 
+use Illuminate\Support\Facades\Config;
+use Fuguevit\Commentable\Contracts\RemarkeInterface;
 
-class RemarkableTrait
+class RemarkableTrait implements RemarkeInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function allRemarks()
+    {
+        $instance = new static();
 
+        return $instance->createRemarksModel()->whereRemarkableType(
+            $instance->getEntityClassName()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remarks()
+    {
+        return $this->morphMany(Config::get('remark.remarkModel'), 'remarkable');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addRemark($userId, $content)
+    {
+        $remark = $this->createRemarksModel()->create([
+            'user_id'   => $userId,
+            'content'   => $content
+        ]);
+        $this->remarks()->attach($remark);
+    }
+
+    /**
+     * Create remarks model.
+     *
+     * @return mixed
+     */
+    protected static function createRemarksModel()
+    {
+        $tagModel = Config::get('remark.remarkModel');
+
+        return new $tagModel();
+    }
+
+    /**
+     * Return the entity class name.
+     *
+     * @return mixed
+     */
+    protected function getEntityClassName()
+    {
+        if (isset(static::$entityNamespace)) {
+            return static::$entityNamespace;
+        }
+        return $this->remarks()->getMorphClass();
+    }
 }
