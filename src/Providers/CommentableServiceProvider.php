3b545ddb510d0cf753e5e4a36bbe6f761b3c0b0e
wangxuan <wangxuan@qgpic.com>
2016-11-02 16:35:24 +0800

    add service provider

---
+++ src/Providers/CommentableServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fuguevit\Commentable\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommentableServiceProvider extends ServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        $this->publishes([
+           realpath(__DIR__.'/../../database/migrations') => database_path('migrations'),
+        ], 'migrations');
+        $this->publishes([
+           realpath(__DIR__.'/../../config/comment.php') => config_path('comment.php'),
+           realpath(__DIR__.'/../../config/remark.php') => config_path('remark.php'),
+        ], 'config');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(realpath(__DIR__.'/../../config/comment.php'), 'comment');
+        $this->mergeConfigFrom(realpath(__DIR__.'/../../config/remark.php'), 'remark');
+    }
+}
