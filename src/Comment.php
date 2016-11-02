4f9d3f19b3ef689169a805c4023dbb762a83e725
wangxuan <wangxuan@qgpic.com>
2016-11-02 12:44:11 +0800

    add comment model & configuration

--- src/Comment.php
+++ src/Comment.php
@@ -2,8 +2,22 @@
 
 namespace Fuguevit\Commentable;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Comment
+class Comment extends Model
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'comments';
 
+    /**
+     * {@inheritdoc}
+     */
+    protected $fillable = ['user_id', 'title', 'body'];
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
 }
