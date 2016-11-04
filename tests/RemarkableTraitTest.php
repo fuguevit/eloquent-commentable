fe15bda6b2a7e39a50333d58e60f2c7bdf0e1459
wangxuan <wangxuan@qgpic.com>
2016-11-04 18:46:49 +0800

    bump

---
+++ tests/RemarkableTraitTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Fuguevit\Commentable\Test;
+
+use Fuguevit\Commentable\Remark;
+
+class RemarkableTraitTest extends TestCase
+{
+    /**
+     * Test remarkable trait can add remark
+     */
+    public function test_add_remark()
+    {
+        $article = $this->createArticle();
+        $article->addRemark(1,'foo');
+
+        $remark = Remark::where('user_id' , 1)->where('content', 'foo')->first();
+        $this->assertInstanceOf('Fuguevit\Commentable\Remark', $remark);
+    }
+}
