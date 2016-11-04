ba6b381feac657aed783d127317c6f87835c7cda
wangxuan <wangxuan@qgpic.com>
2016-11-04 12:44:10 +0800

    add phpunit test

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Fuguevit\Commentable\Test;
+
+use Illuminate\Database\Schema\Blueprint;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Fuguevit\Commentable\Test\Models\Article;
+
+abstract class TestCase extends OrchestraTestCase
+{
+
+}
