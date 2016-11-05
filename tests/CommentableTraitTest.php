053457fa83099679b427fc0dda88b30cc3fc7db1
fugue <wangxuan@188.com>
2016-11-05 21:34:48 +0800

    add tests

--- tests/CommentableTraitTest.php
+++ tests/CommentableTraitTest.php
@@ -9,12 +9,21 @@ class CommentableTraitTest extends TestCase
     /**
      * Test commentable trait can add comment.
      */
-    public function test_add_comment()
+    public function test_add_comment_function_can_create_a_new_comment()
     {
         $article = $this->createArticle();
         $article->addComment(1, 'foo', 'bar');
 
         $comment = Comment::where('user_id', 1)->where('title', 'foo')->first();
         $this->assertInstanceOf('Fuguevit\Commentable\Comment', $comment);
+    }
+
+    /**
+     * Test comments function returns morphMany.
+     */
+    public function test_comments_function_has_morph_many_relation_result()
+    {
+        $article  = $this->createArticle();
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Relations\MorphMany', $article->comments());
     }
 }
