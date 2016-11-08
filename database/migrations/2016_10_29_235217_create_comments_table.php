939ccd38f3110e0fd869590320bc3a4123d54ac2
fugue <wangxuan@188.com>
2016-11-08 02:56:17 +0000

    Applied fixes from StyleCI

--- database/migrations/2016_10_29_235217_create_comments_table.php
+++ database/migrations/2016_10_29_235217_create_comments_table.php
@@ -12,7 +12,7 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function(Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->engine = 'InnoDB';
 
             $table->increments('id');
